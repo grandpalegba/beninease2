4ab1325586e9c3ef225510288ec3ad59b3224200
Roméo Nonvidé <romeononvide@iMac-de-Romeo.local>
2026-04-28 09:54:26 -0500

    feat: remove HeroSection, make AfrakaDoors the primary landing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
  * PAGE PUBLIQUE - ACCUEIL
  * Role: Vitrine principale du site BeninEase.
  */
-import HeroSection from "@/components/HeroSection";
 import { AfrakaDoors } from "@/components/AfrakaDoors";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <main>
-        <HeroSection />
         <AfrakaDoors />
       </main>
     </div>
