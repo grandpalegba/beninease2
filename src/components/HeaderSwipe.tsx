cb70162bac3c7dc11c3d6674597a786c7dcca4cf
Roméo Nonvidé <romeononvide@iMac-de-Romeo.local>
2026-05-02 16:05:40 -0500

    UI: Update 'To' color to black and swap Visions/Trésors icons

--- src/components/HeaderSwipe.tsx
+++ src/components/HeaderSwipe.tsx
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils";
 import { CategoryPattern } from "@/components/talents/CategoryPattern";
 
 const PAGES = [
-  { name: "Visions", href: "/visions", id: "startup-innovation" },
-  { name: "Trésors", href: "/tresors", id: "vision" },
+  { name: "Visions", href: "/visions", id: "vision" },
+  { name: "Trésors", href: "/tresors", id: "startup-innovation" },
   { name: "Histoires", href: "/histoires", id: "mythes-legendes" },
   { name: "Savoirs", href: "/savoirs", id: "sagesse" },
   { name: "Sagesses", href: "/sagesses", id: "parole-aines" },
